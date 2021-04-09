962dae192fec9b9617390ee8fa9e51e70087acd2
MaximeB <bene.max31@gmail.com>
2021-04-09 13:57:13 +0200

    Create musique.js

---
+++ BACK/modif/beansMax/musique.js
@@ -0,0 +1,18 @@
+class Musique {
+    
+    idMus
+    nom
+    jaime
+    nbrJouer
+   
+    constructor(nom){
+        this.idMus= null
+        this.nom = nom
+        this.jaime = null
+        this.nbrJouer = null
+    }
+}
+
+module.exports = Musique
+
+
