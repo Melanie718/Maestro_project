582dcb962906fbd669d0e0133ef4a146335c83da
MaximeB <bene.max31@gmail.com>
2021-04-09 13:57:15 +0200

    Create niveau.js

---
+++ BACK/modif/beansMax/niveau.js
@@ -0,0 +1,22 @@
+class Niveau {
+    
+    idNiv
+    idEns
+    nom
+    nbrUtilisation
+    jaime
+   
+    constructor(idEns,nom,nbrUtilisation,jaime){
+        this.idNiv = null
+        this.idEns = idEns
+        this.nom = nom
+        this.nbrUtilisation = nbrUtilisation
+        this.jaime = jaime
+    }
+}
+
+module.exports = Niveau
+
+
+
+
